79a11387de44ab7813c8c7cf5d9fae32cede3e6e
EMBSKD <arnaudlejeune44@gmail.com>
2019-06-11 11:02:44 +0200

    Added Attribute box on map and both graphs (html, css only, no js interaction yet) + added comments on css & HTML for better code comprehension

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
 
 app.secret_key = "TODO: mettre une valeur secrète ici"
 session_id_init = "hds62bbddsb0b35"
 
+# Initialisation function
+def init():
+	session['map_attributes'] = ['Gini', 'Income', 'PIB']
+	session['graph_attributes'] = ['Gini', 'Wealth', 'Test']
+
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', title="Inequalities in the world ")
-
+	init()
+	return render_template('index.html', title="Inequalities in the world ")
+	
 @app.route('/canvas', methods=['GET'])
 def canvas():
-    return render_template('canvasV0.2.html')
+	init()
+	return render_template('canvasV0.2.html', map_attributes = session['map_attributes'], graph_attributes = session['graph_attributes'])
 
 if __name__ == '__main__':
     app.run(debug=True)
