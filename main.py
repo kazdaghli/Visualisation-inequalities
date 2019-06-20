063049c4e78f59c7b5413002ea61f4027d59b089
Mickael <lopesmickael8@gmail.com>
2019-06-20 09:14:10 +0200

    New style v0.3

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def index():
 @app.route('/canvas', methods=['GET'])
 def canvas():
 	init()
-	return render_template('canvasV0.2.html', map_attributes = session['map_attributes'], graph_attributes = session['graph_attributes'])
+	return render_template('canvasV0.3.html', map_attributes = session['map_attributes'], graph_attributes = session['graph_attributes'])
 
 if __name__ == '__main__':
     app.run(debug=True)
